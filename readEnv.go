e9cc2393a1374a3f462f4a2ec5e2bff87f079501
christian <christian.avz@gmail.com>
2021-08-01 22:52:10 -0400

    readStruct

--- readEnv.go
+++ readEnv.go
@@ -8,11 +8,26 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func ReadEnv() {
+type Wea struct {
+	Default bool
+	User    interface{}
+}
+
+type User struct {
+	Nombre string
+}
+
+func (w *Wea) ReadEnv() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 
 	}
 	fmt.Printf("os.Getenv(\"MESSAGE\"): %v\n", os.Getenv("MESSAGE"))
+	if w.Default {
+		fmt.Println(w.User)
+	} else {
+		user := User{}
+		fmt.Printf("User: %v\n", user)
+	}
 }
